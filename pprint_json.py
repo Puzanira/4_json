8e473ce68db6a5174028e74dd8c0ea7aaabb3fad
Puzanira <puzanova.ira2011@yandex.ru>
2018-01-22 22:19:29 +0300

    Добавлен комментарий

--- pprint_json.py
+++ pprint_json.py
@@ -3,23 +3,24 @@ import sys
 
 
 def load_from_json(filepath):
-    
-# в официальной документации python сказано, что with open действует аналогично блоку
-# try fiinally, и также выкинет ошибку, если его не существует
-# проверка уже встроена, ничего не сломается
-
+    '''
+    в официальной документации python сказано, что with open
+    действует аналогично блоку
+    try finally и также выкинет ошибку, если его не существует
+    проверка уже встроена
+    '''
     with open(filepath, 'r') as file_handler:
         return json.load(file_handler)
 
 
 def pretty_print_json(json_data):
-    print(json.dumps(json_data, indent=4, ensure_ascii = False))
+    print(json.dumps(json_data, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print ("Error: define file path")
-        print ("Usage example: python pprint_json.py <path to file>")
+        print('Error: define file path')
+        print('Usage example: python pprint_json.py <path to file>')
     else:
         json_data = load_from_json(sys.argv[1])
         pretty_print_json(json_data)
